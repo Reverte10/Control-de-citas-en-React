a2a78c535a51153b8acce56667ec4ea175606ed3
Reverte10 <sergioreverte10@gmail.com>
2023-11-29 09:55:13 +0100

    creado formulario

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import ListadoPacientes from "./components/ListadoPacientes"
 function App() {
 
   return (
-    <>
+    <div className="container mx-auto mt-10">
       <Header />
-      <ListadoPacientes />
-      <Formulario />
-    </>
+
+      <div className="mt-12 md:flex">
+        <Formulario /> 
+        <ListadoPacientes />
+      </div>
+      
+    </div>
   )
 }
 
