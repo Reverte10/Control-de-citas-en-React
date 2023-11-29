a7750670597b3db262d8c109fc9cce120c92fdee
Reverte10 <sergioreverte10@gmail.com>
2023-11-29 12:51:19 +0100

    creado props

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,24 @@
 import { useState, useEffect } from 'react';
 import Error from './Error';
 
-function Formulario({ pacientes, setPacientes }) {
+function Formulario({ pacientes, setPacientes, paciente }) {
   const [nombre, setNombre] = useState('');
   const [propietario, setPropietario] = useState('');
   const [email, setEmail] = useState('');
   const [fecha, setFecha] = useState('');
   const [sintomas, setSintomas] = useState('');
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+
+  }, [paciente])
+
+  const generarId = () => {
+    const random = Math.random().toString(36).substr(2);
+    const fecha = Date.now().toString(36)
+
+    return random + fecha
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +35,8 @@ function Formulario({ pacientes, setPacientes }) {
       propietario,
       email,
       fecha,
-      sintomas
+      sintomas,
+      id : generarId()
     }
 
     setPacientes([...pacientes, objetoPaciente])
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,25 +1,41 @@
-const Paciente = () => {
+const Paciente = ({paciente, setPaciente}) => {
+    const {nombre, propietario, email, fecha, sintomas} = paciente
+
+
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {' '}
-                <span className="font-normal normal-case">Thor</span>
+                <span className="font-normal normal-case">{nombre}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {' '}
-                <span className="font-normal normal-case">Sergio</span>
+                <span className="font-normal normal-case">{propietario}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Email: {' '}
-                <span className="font-normal normal-case">sergioreverte10@gmail.com</span>
+                <span className="font-normal normal-case">{email}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Alta: {' '}
-                <span className="font-normal normal-case">20/05/2019</span>
+                <span className="font-normal normal-case">{fecha}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {' '}
-                <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, explicabo. Veniam sunt rem nihil aspernatur minus a nulla odio. Possimus nisi praesentium sapiente iure. Illo quos quis accusantium voluptatum delectus.</span>
+                <span className="font-normal normal-case">{sintomas}</span>
             </p>
+
+            <div className="flex justify-between mt-10">
+                <button type="button"
+                className="py-2 px-8 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                onClick={() => setPaciente(paciente)}>
+                    Editar
+                </button>
+
+                <button type="button"
+                className="py-2 px-8 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">
+                    Eliminar
+                </button>
+            </div>
 
         </div>
     )
