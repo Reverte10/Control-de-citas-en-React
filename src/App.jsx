f79fd810e30d066363e851e0a398a819cad51f90
Reverte10 <sergioreverte10@gmail.com>
2023-11-29 11:57:37 +0100

    creado props de formulario a app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react'
 import Header from "./components/header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
+  const [pacientes, setPacientes] = useState([])
+
   return (
     <div className="container mx-auto mt-10">
       <Header />
 
       <div className="mt-12 md:flex">
-        <Formulario /> 
-        <ListadoPacientes />
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+        />
+        <ListadoPacientes
+
+        />
       </div>
-      
+
     </div>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,55 @@
-function Formulario() {
+import { useState, useEffect } from 'react';
+import Error from './Error';
+
+function Formulario({ pacientes, setPacientes }) {
+  const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, propietario, email, fecha, sintomas].includes('')) {
+      setError(true)
+      return;
+    }
+
+    setError(false)
+
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas
+    }
+
+    setPacientes([...pacientes, objetoPaciente])
+
+    setNombre ('')
+    setPropietario ('')
+    setEmail ('')
+    setFecha ('')
+    setSintomas ('')
+
+  }
+
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-xl mt-5 text-center mb-10">
         Añade paciente y {' '}
         <span className="text-indigo-600 font-bold">Administralo</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        {error && <Error mensaje = 'Todos los campos son obligatorios'/>}
         <div className="mb-5">
           <label htmlFor='mascota' className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input
@@ -15,6 +57,8 @@ function Formulario() {
             type="text"
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
 
@@ -25,6 +69,8 @@ function Formulario() {
             type="text"
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={propietario}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
 
@@ -35,6 +81,8 @@ function Formulario() {
             type="email"
             placeholder="Email Contacto propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -44,6 +92,8 @@ function Formulario() {
             id='alta'
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={fecha}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
 
@@ -53,6 +103,8 @@ function Formulario() {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 "
             id="sintomas"
             placeholder="Describe los Síntomas"
+            value={sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
           />
 
         </div>
